6f9d3239ad155198fdc0ab2f81f3182fddfcc0f7
pradeep-selva <pradeepselvaraju@gmail.com>
2025-01-30 00:27:21 +0530

    ft: getAddress viem

--- src/constants.ts
+++ src/constants.ts
@@ -1,20 +1,19 @@
-import { ethers } from "ethers";
-import { Address } from "viem";
+import { Address, getAddress } from "viem";
 
 export const ADDRESSES = {
   PROD: {
-    EXECUTOR_PLUGIN: ethers.utils.getAddress(
+    EXECUTOR_PLUGIN: getAddress(
       "0xeC4181eE959e47f72e9cC60274fBE53D68949e47"
     ) as Address,
-    MULTI_SEND: ethers.utils.getAddress(
+    MULTI_SEND: getAddress(
       "0x40A2aCCbd92BCA938b02010E17A5b8929b49130D"
     ) as Address
   },
   DEV: {
-    EXECUTOR_PLUGIN: ethers.utils.getAddress(
+    EXECUTOR_PLUGIN: getAddress(
       "0xb92929d03768a4F8D69552e15a8071EAf8E684ed"
     ) as Address,
-    MULTI_SEND: ethers.utils.getAddress(
+    MULTI_SEND: getAddress(
       "0x40A2aCCbd92BCA938b02010E17A5b8929b49130D"
     ) as Address
   }
